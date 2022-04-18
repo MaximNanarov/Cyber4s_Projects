aa3826af7d8acd6c39e549aeb793d7a13e1cd6dd
MaximNanarov <102975521+MaximNanarov@users.noreply.github.com>
2022-04-18 17:29:10 +0300

    the chessboard is complete

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 // When user clicks, show possible movements by a different color, without worrying about other pieces (as if the piece was along on the board).
 
 const BOARD_SIZE = 8;
-const WHITE_PLAYER = 'white';
+const WHITE_PLAYER = 'white_rotated';
 const DARK_PLAYER = 'dark';
 
 let selectedCell;
@@ -18,15 +18,51 @@ class Piece {
 }
 
 function getInitialBoard() {
-  let result = [];
+
+  let result = [];                                    // the white pieces
   result.push(new Piece(0, 0, "rook", WHITE_PLAYER))
   result.push(new Piece(0, 1, "knight", WHITE_PLAYER))
+  result.push(new Piece(0, 2, "bishop", WHITE_PLAYER))
+  result.push(new Piece(0, 3, "queen", WHITE_PLAYER))
+  result.push(new Piece(0, 4, "king", WHITE_PLAYER)) 
+  result.push(new Piece(0, 5, "bishop", WHITE_PLAYER))
+  result.push(new Piece(0, 6, "knight", WHITE_PLAYER))
+  result.push(new Piece(0, 7, "rook", WHITE_PLAYER))
+
+  result.push(new Piece(7, 0, "rook", DARK_PLAYER))
+  result.push(new Piece(7, 1, "knight", DARK_PLAYER))
+  result.push(new Piece(7, 2, "bishop", DARK_PLAYER))
+  result.push(new Piece(7, 3, "queen", DARK_PLAYER))
+  result.push(new Piece(7, 4, "king", DARK_PLAYER)) 
+  result.push(new Piece(7, 5, "bishop", DARK_PLAYER))
+  result.push(new Piece(7, 6, "knight", DARK_PLAYER))
+  result.push(new Piece(7, 7, "rook", DARK_PLAYER))
+  // let piecesName = ['rook','knight','bishop','queen', 'king','bishop','knight','rook'];
+  // result.push(SetStartingPieces(piecesName, WHITE_PLAYER, result))
+  for(let i = 0; i<8; i++){
+  result.push(new Piece(1,i,"pawn", WHITE_PLAYER));
+  result.push(new Piece(6,i,"pawn", DARK_PLAYER));
+  }
+
   return result;
 }
 
+// function SetStartingPieces(piecesName, typePLayer, result)
+// {
+//   let piecesNameLength = piecesName.Length; 
+//   for(let i = 0; i < piecesNameLength; i++)
+//   {
+//     if(typePLayer !== 'dark')
+//     result.push(new Piece(0, i, piecesName[i], typePLayer))
+//     else
+//     result.push(new Piece(7, i, piecesName[i], typePLayer))
+//   }
+//   return result;
+// }
+
 function addImage(cell, player, name) {
   const image = document.createElement('img');
-  image.src = 'images/' + player + '/' + name + '.png';
+  image.src = 'images/' + player + '/' + name + '.png';   //images\white_rotated\knight.png
   cell.appendChild(image);
 }
 
@@ -43,15 +79,18 @@ function addImageByIndex(cell, player, index) {
     addImage(cell, player, 'queen');
   }
 }
-
+//the click that chooses a cell
 function onCellClick(event) {
   if (selectedCell !== undefined) {
     selectedCell.classList.remove('selected');
   }
   selectedCell = event.currentTarget;
   selectedCell.classList.add('selected');
-}
-
+  console.log(selectedCell);
+  
+  
+  }
+// creats the chess board
 function createChessBoard() {
   const table1 = document.createElement('table');
   document.body.appendChild(table1);
@@ -71,8 +110,11 @@ function createChessBoard() {
   pieces = getInitialBoard();
 
   for (let piece of pieces) {
+    
     addImage(table1.rows[piece.row].cells[piece.col], piece.player, piece.type);
   }
 }
 
 window.addEventListener('load', createChessBoard);
+
+
